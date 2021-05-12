b1e15600ca0375e57ca8d5cb25240332154fbb7b
Leon Kamuf <kamuf@elform.net>
2021-05-12 09:12:02 +0200

    Initial commit

---
+++ ExternalSample/Program.cs
@@ -0,0 +1,42 @@
+﻿#region Codeconflict
+
+using Org.BouncyCastle.Utilities.Encoders;
+
+namespace ExternalSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            byte[] testDecode = Base64.Decode("Test");
+
+        }
+    }
+}
+
+#endregion
+
+
+//#region External-Version
+
+////In der Verweisauflistung im Projekt auf "itextsharp" gehen und Aliase auf "iText" setzen
+
+//extern alias iText;
+
+//using Org.BouncyCastle.Utilities.Encoders;
+
+//namespace ExternalSample
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+//            byte[] testDecode = Base64.Decode("Test");
+
+//        }
+//    }
+//}
+
+//#endregion
